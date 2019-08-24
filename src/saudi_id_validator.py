76cf3a3d1336d7f6abb7d654bd3d8aefc6bd2ed0
Mohammed H Alshehri <542855+mhalshehri@users.noreply.github.com>
2019-08-24 22:13:01 +0300

    improve argument type hint

--- src/saudi_id_validator.py
+++ src/saudi_id_validator.py
@@ -1,8 +1,11 @@
-def validate(id_number: int) -> bool:
+from typing import Union
+
+
+def validate(id_number: Union[int, str]) -> bool:
     """Validate format and checksum of the identity number.
 
     :param id_number: Saudi Arabian identity number
-    :type id_number: int
+    :type id_number: int or str
     """
 
     digits = [int(d) for d in str(id_number)]
